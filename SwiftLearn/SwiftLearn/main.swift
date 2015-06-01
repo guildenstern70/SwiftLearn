78b98c68c9e4a2fe86f2953e3b8e033ce6d02757
guildenstern <alessiosaltarin@gmail.com>
2015-06-01 20:45:06 +0200

    Initial Commit

---
+++ SwiftLearn/SwiftLearn/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLearn
+//
+//  Created by Alessio Saltarin on 01/06/15.
+//  Copyright (c) 2015 Alessio Saltarin. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
